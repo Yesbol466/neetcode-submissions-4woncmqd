a510a733ab50785f27176d108d184ab193cb8ee7
Yesbol Yerlan <liqiwang5@gmail.com>
2026-04-20 22:39:00 +0200

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] cache = new int[n+1];
+
+        if(n < 2) return n;
+
+        cache[0] = 1;
+        cache[1] = 2;
+
+        for(int i = 2; i<n; i++){
+            cache[i] = cache[i-1] + cache[i-2]; 
+        }
+
+        return cache[n-1];
+    }
+}
