1132d28ffea7bcd9d268e43bfb59b6d4ba6b7106
Yesbol Yerlan <liqiwang5@gmail.com>
2026-07-19 14:08:46 +0200

    Add: find-anagram-mappings - submission-3

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        result = [0] * len(nums1)
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    result[i] = j
+                    break
+        return result 
+        
