89de635d5f6d00d48b1aca15b65d9bf5af1aa03a
Yesbol Yerlan <liqiwang5@gmail.com>
2026-08-03 22:37:38 +0200

    Add: lonely-pixel-i - submission-0

---
+++ Data Structures & Algorithms/lonely-pixel-i/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        n = len(picture)
+        m = len(picture[0])
+        row_count = [0] * n
+        col_count = [0] * m
+        for i in range(n):
+            for j in range(m):
+                if picture[i][j] == 'B':
+                    row_count[i] +=1
+                    col_count[j] +=1
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if picture[i][j] == 'B' and row_count[i] == 1 and col_count[j] == 1:
+                    ans+=1
+        return ans
+        
+        
+        
+        
